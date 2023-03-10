7c409c0e079756d17b8e9195bf748d55357477b2
Ryan Dale <ryan.dale@nih.gov>
2023-03-10 11:47:28 -0500

    simplify & update comments

--- install-and-set-up-conda.sh
+++ install-and-set-up-conda.sh
@@ -2,39 +2,28 @@
 
 set -e
 
-# - Installs miniconda to ${HOME}/miniconda
-# - Sets channels
+# - Installs miniconda to ${HOME}/miniconda. Version is determined by common.sh,
+#   which is downloaded at runtime.
+# - Sets channel order and sets strict channel priority
 # - Installs mamba into the base env
-# - Installs bioconda-utils into a "bioconda" env.
+# - Installs bioconda-utils into a "bioconda" env (unless
+#   $BIOCONDA_DISABLE_BUILD_PREP=1). Version is determined by common.sh.
 # - Sets up local channel to have highest priority (unless $BIOCONDA_DISABLE_BUILD_PREP=1)
-#
-# Intended to be run from GitHub Action or Azure Pipeline or CircleCI steps.
-# You'll want to also have a step early on in a job that sets the path, like
-# this, (for GitHub Actions):
-#
-# jobs:
-#   job-name-1:
-#     name: ...
-#     runs-on: ...
-#     steps:
-#       - name: set path
-#         run: echo "/miniconda/bin" >> $GITHUB_PATH
-#       - name: ...
 
 # Extract the versions we should be using from common.sh
 curl -L "https://raw.githubusercontent.com/bioconda/bioconda-common/master/common.sh" > common.sh
 BIOCONDA_UTILS_TAG=$(grep "^BIOCONDA_UTILS_TAG=" common.sh | cut -f2 -d "=" | sed "s/^v//g")
 MINICONDA_VER=$(grep "^MINICONDA_VER=" common.sh | cut -f2 -d "=")
-
+MINICONDA_INSTALLATION_DIR="${HOME}/miniconda"
 
 if [[ $(uname) == "Darwin" ]]; then
     OS="MacOSX"
 
     # Remove existing installation on macOS runners
     sudo rm -rf /usr/local/miniconda
-    sudo rm -rf ${HOME}/miniconda
+    sudo rm -rf ${MINICONDA_INSTALLATION_DIR}
 
-    # conda-forge-ci-setup does some additional setup.
+    # conda-forge-ci-setup does some additional setup for Mac.
     # Installing bioconda-utils and conda-forge-ci-setup with conda causes dependency conflicts.
     # Installing bioconda-utils and conda-forge-ci-setup with mamba works fine.
     BIOCONDA_ADDITIONAL_INSTALL_PKGS="conda-forge-ci-setup"
@@ -46,9 +35,9 @@ fi
 
 # Install miniconda
 curl -L "https://repo.anaconda.com/miniconda/Miniconda3-${MINICONDA_VER}-${OS}-x86_64.sh" > miniconda.sh
-bash miniconda.sh -b -p "${HOME}/miniconda"
+bash miniconda.sh -b -p "${MINICONDA_INSTALLATION_DIR}"
 
-export PATH="${HOME}/miniconda/bin:${PATH}"
+export PATH="${MINICONDA_INSTALLATION_DIR}/bin:${PATH}"
 
 # Set up channels
 conda config --set always_yes yes
@@ -67,19 +56,19 @@ if [ ${BIOCONDA_DISABLE_BUILD_PREP:=0} == 0 ]; then
 
     mamba create -n bioconda -y bioconda-utils=$BIOCONDA_UTILS_TAG $BIOCONDA_ADDITIONAL_INSTALL_PKGS
 
-    source ${HOME}/miniconda/etc/profile.d/conda.sh
+    source ${MINICONDA_INSTALLATION_DIR}/etc/profile.d/conda.sh
     conda activate bioconda
 
     # Set local channel as highest priority (requires conda-build, which is
     # installed as a dependency of bioconda-utils)
-    mkdir -p "${HOME}/miniconda/conda-bld/{noarch,linux-64,osx-64}"
-    conda index "${HOME}/miniconda/conda-bld"
-    conda config --system --add channels "file://${HOME}/miniconda/conda-bld"
+    mkdir -p "${MINICONDA_INSTALLATION_DIR}/conda-bld/{noarch,linux-64,osx-64}"
+    conda index "${MINICONDA_INSTALLATION_DIR}/conda-bld"
+    conda config --system --add channels "file://${MINICONDA_INSTALLATION_DIR}/conda-bld"
 fi
 
+echo "=========="
 echo "conda config:"
 conda config --show
-
-echo "conda installed to $(which conda)"
+echo "=========="
 echo "environment(s): $(conda env list)"
-echo "DONE setting up"
+echo "DONE setting up via $0"
