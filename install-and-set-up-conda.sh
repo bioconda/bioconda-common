ecbdd6334ea6ef691d114c513b8126b6302934f1
Devon Ryan <dpryan79@users.noreply.github.com>
2023-05-03 20:09:49 +0200

    Update install-and-set-up-conda.sh

--- install-and-set-up-conda.sh
+++ install-and-set-up-conda.sh
@@ -53,6 +53,9 @@ conda config --set always_yes yes
 conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
+conda config --system --add channels defaults
+conda config --system --add channels bioconda
+conda config --system --add channels conda-forge
 conda config --set channel_priority strict
 mamba info
 
