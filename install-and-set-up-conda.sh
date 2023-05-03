706f7fb44fa3ad8b14330b2be57ac08cf7f6bc11
Johannes Köster <johannes.koester@tu-dortmund.de>
2023-05-03 09:42:18 +0200

    perf: use mambaforge instead of miniconda, add test, use bioconda-utils 1.6.0 (#35)
    * perf: use mambaforge instead of miniconda, add test
    
    * run script
    
    * omit download
    
    * dbg
    
    * use specific sha
    
    * dbg
    
    * dbg
    
    * fix
    
    * fix
    
    * fixes
    
    * Use bioconda-utils 1.6.0
    
    * also test bioconda-utils installation
    
    * fixes
    
    * also source conda.sh

--- install-and-set-up-conda.sh
+++ install-and-set-up-conda.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# - Installs miniconda to ${HOME}/miniconda. Version is determined by common.sh,
+# - Installs mambaforge to ${HOME}/mambaforge. Version is determined by common.sh,
 #   which is downloaded at runtime.
 # - Sets channel order and sets strict channel priority
 # - Installs mamba into the base env
@@ -11,37 +11,42 @@ set -e
 # - Sets up local channel to have highest priority (unless $BIOCONDA_DISABLE_BUILD_PREP=1)
 
 # Extract the versions we should be using from common.sh
-curl -L "https://raw.githubusercontent.com/bioconda/bioconda-common/master/common.sh" > common.sh
+COMMON_GIT_REF=${COMMON_GIT_REF:-master}
+curl -L "https://raw.githubusercontent.com/bioconda/bioconda-common/${COMMON_GIT_REF}/common.sh" > common.sh
+cat common.sh
+
 BIOCONDA_UTILS_TAG=$(grep "^BIOCONDA_UTILS_TAG=" common.sh | cut -f2 -d "=" | sed "s/^v//g")
-MINICONDA_VER=$(grep "^MINICONDA_VER=" common.sh | cut -f2 -d "=")
-MINICONDA_INSTALLATION_DIR="/opt/miniconda"
+MAMBAFORGE_VER=$(grep "^MAMBAFORGE_VER=" common.sh | cut -f2 -d "=")
+MAMBAFORGE_INSTALLATION_DIR="/opt/mambaforge"
 ARCH=$(uname -m)
+MAMBAFORGE_URL="https://github.com/conda-forge/miniforge/releases/download/${MAMBAFORGE_VER}/Mambaforge-${MAMBAFORGE_VER}-Linux-x86_64.sh"
 
 if [[ $(uname) == "Darwin" ]]; then
     OS="MacOSX"
-
+    
     # Remove existing installation on macOS runners
-    sudo rm -rf /usr/local/miniconda
-    sudo rm -rf ${MINICONDA_INSTALLATION_DIR}
-    sudo mkdir -p $(dirname $MINICONDA_INSTALLATION_DIR)
-    sudo chown -R $USER $(dirname $MINICONDA_INSTALLATION_DIR)
-
+    sudo rm -rf ${MAMBAFORGE_INSTALLATION_DIR}
+    sudo mkdir -p $(dirname $MAMBAFORGE_INSTALLATION_DIR)
+    sudo chown -R $USER $(dirname $MAMBAFORGE_INSTALLATION_DIR)
+    
     # conda-forge-ci-setup does some additional setup for Mac.
     # Installing bioconda-utils and conda-forge-ci-setup with conda causes dependency conflicts.
     # Installing bioconda-utils and conda-forge-ci-setup with mamba works fine.
     BIOCONDA_ADDITIONAL_INSTALL_PKGS="conda-forge-ci-setup"
 else
-    mkdir -p $(dirname $MINICONDA_INSTALLATION_DIR)
+    mkdir -p $(dirname $MAMBAFORGE_INSTALLATION_DIR)
     OS="Linux"
     BIOCONDA_ADDITIONAL_INSTALL_PKGS=""
 fi
 
 
-# Install miniconda
-curl -L "https://repo.anaconda.com/miniconda/Miniconda3-${MINICONDA_VER}-${OS}-${ARCH}.sh" > miniconda.sh
-bash miniconda.sh -b -p "${MINICONDA_INSTALLATION_DIR}"
+# Install mambaforge
+echo Download ${MAMBAFORGE_URL}
+curl -L ${MAMBAFORGE_URL} > mambaforge.sh
+head mambaforge.sh
+bash mambaforge.sh -b -p "${MAMBAFORGE_INSTALLATION_DIR}"
 
-export PATH="${MINICONDA_INSTALLATION_DIR}/bin:${PATH}"
+export PATH="${MAMBAFORGE_INSTALLATION_DIR}/bin:${PATH}"
 
 # Set up channels
 conda config --set always_yes yes
@@ -49,25 +54,26 @@ conda config --system --add channels defaults
 conda config --system --add channels bioconda
 conda config --system --add channels conda-forge
 conda config --system --set channel_priority strict
-conda info
+mamba info
 
-conda install mamba -y
+mamba install mamba -y
 
 # By default, for building packages, we install bioconda-utils. However when
 # testing bioconda-utils itself, we don't want to install it from conda, in
 # which case set BIOCONDA_DISABLE_BUILD_PREP to a non-zero value.
 if [ ${BIOCONDA_DISABLE_BUILD_PREP:=0} == 0 ]; then
-
+    
     mamba create -n bioconda -y bioconda-utils=$BIOCONDA_UTILS_TAG $BIOCONDA_ADDITIONAL_INSTALL_PKGS
-
-    source ${MINICONDA_INSTALLATION_DIR}/etc/profile.d/conda.sh
-    conda activate bioconda
-
+    
+    source ${MAMBAFORGE_INSTALLATION_DIR}/etc/profile.d/conda.sh
+    source ${MAMBAFORGE_INSTALLATION_DIR}/etc/profile.d/mamba.sh
+    mamba activate bioconda
+    
     # Set local channel as highest priority (requires conda-build, which is
     # installed as a dependency of bioconda-utils)
-    mkdir -p "${MINICONDA_INSTALLATION_DIR}/conda-bld/{noarch,linux-64,osx-64}"
-    conda index "${MINICONDA_INSTALLATION_DIR}/conda-bld"
-    conda config --system --add channels "file://${MINICONDA_INSTALLATION_DIR}/conda-bld"
+    mkdir -p "${MAMBAFORGE_INSTALLATION_DIR}/conda-bld/{noarch,linux-64,osx-64}"
+    conda index "${MAMBAFORGE_INSTALLATION_DIR}/conda-bld"
+    conda config --system --add channels "file://${MAMBAFORGE_INSTALLATION_DIR}/conda-bld"
 fi
 
 echo "=========="
