f5b434bc9386df8adc107651f6c109db2681857d
Johannes Köster <johannes.koester@uni-due.de>
2023-05-04 09:26:09 +0200

    documentation

--- configure-conda.sh
+++ configure-conda.sh
@@ -1,3 +1,10 @@
+# This script can be used to reconfigure conda to use the right channel setup.
+# This has to be done after the cache is restored, because
+# the channel setup is not cached as it resides in the home directory.
+# We could use a system-wide (and therefore cached) channel setup,
+# but mamba does not support that at the time of implementation
+# (it ignores settings made with --system).
+
 MAMBAFORGE_INSTALLATION_DIR="/opt/mambaforge"
 
 conda config --set always_yes yes
