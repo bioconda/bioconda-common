6038711185ef59711f34a6bdf5ae7851058b24fd
Ryan Dale <ryan.dale@nih.gov>
2024-06-08 15:37:27 +0000

    export macOS deployment targets directly
    rather than depend on
    bioconda-utils:bioconda_utils/bioconda-utils_conda-build-config.yaml,
    which does not appear to export these as env vars for
    `run_conda_forge_build_setup` to use.

--- common.sh
+++ common.sh
@@ -1,3 +1,18 @@
 BIOCONDA_UTILS_TAG=v3.3.1
 MAMBAFORGE_VER=24.3.0-0
 MAMBAFORGE_INSTALLATION_DIR="/opt/mambaforge"
+
+# While these are specified in
+# bioconda-utils:bioconda_utils/bioconda_utils-conda_build_config.yaml,
+# they do not make it out into environment variables. Yet they are required for
+# run_conda_forge_build_setup to set the correct deployment target.
+platform=$(uname -s)
+arch=$(uname -m)
+if [[ "$platform" == "Darwin" ]]; then
+    if [[ "$arch" == "arm64" ]]; then
+        MACOSX_DEPLOYMENT_TARGET=11
+    elif [[ "$arch" == 'x86_64' ]] then
+        MACOSX_DEPLOYMENT_TARGET=10.13
+        MACOSX_DEPLOYMENT_TARGET=10.13
+    fi
+fi
