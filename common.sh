b68da1a29e06e3867ab0e3a64c66e82c3e2a9320
Ryan Dale <ryan.dale@nih.gov>
2024-06-08 21:38:30 +0000

    export macOS env vars
    While these are specified in bioconda_utils'
    bioconda_utils-conda_build_config.yaml, they do not make it out into
    environment variables. Yet they are required for
    `run_conda_forge_build_setup` to set the correct deployment target, so
    we set them here.

--- common.sh
+++ common.sh
@@ -1,3 +1,14 @@
 BIOCONDA_UTILS_TAG=v3.3.1
 MAMBAFORGE_VER=24.3.0-0
 MAMBAFORGE_INSTALLATION_DIR="/opt/mambaforge"
+platform=$(uname -s)
+arch=$(uname -m)
+if [[ "$platform" == "Darwin" ]]; then
+    if [[ "$arch" == "arm64" ]]; then
+        export MACOSX_DEPLOYMENT_TARGET=11
+        export MACOSX_SDK_VERSION=11
+    elif [[ "$arch" == 'x86_64' ]]; then
+        export MACOSX_DEPLOYMENT_TARGET=10.13
+        export MACOSX_SDK_VERSION=10.13
+    fi
+fi
