9cc799f7c3a5e4ece7475b0252dc1ac8e40f2856
Joshua Zhuang <71105179+mencian@users.noreply.github.com>
2025-07-17 14:16:03 -0500

    Convert to miniforge, take 2 (#73)

--- common.sh
+++ common.sh
@@ -1,7 +1,7 @@
 # Respect overrides from environment
 BIOCONDA_UTILS_TAG=${BIOCONDA_UTILS_TAG:="v3.7.2"}
-MAMBAFORGE_VER=${MAMBAFORGE_VER:="24.11.3-2"}
-MAMBAFORGE_INSTALLATION_DIR=${MAMBAFORGE_INSTALLATION_DIR:="/opt/mambaforge"}
+MINIFORGE_VER=${MINIFORGE_VER:="25.3.0-3"}
+MINIFORGE_INSTALLATION_DIR=${MINIFORGE_INSTALLATION_DIR:="/opt/mambaforge"}
 platform=$(uname -s)
 arch=$(uname -m)
 if [[ "$platform" == "Darwin" ]]; then
--- install-and-set-up-conda.sh
+++ install-and-set-up-conda.sh
@@ -1,8 +1,7 @@
-#! /bin/bash
-
+#!/bin/bash
 set -e
 
-# - Installs mambaforge to ${HOME}/mambaforge. Version is determined by common.sh,
+# - Installs miniforge to ${HOME}/mambaforge. Version is determined by common.sh,
 #   which is downloaded at runtime.
 # - Sets channel order and sets strict channel priority
 # - Installs bioconda-utils into a "bioconda" env (unless
@@ -22,7 +21,7 @@ done
 source common.sh
 
 # assert that common.sh has set the variables we need
-for var in BIOCONDA_UTILS_TAG MAMBAFORGE_VER MAMBAFORGE_INSTALLATION_DIR
+for var in BIOCONDA_UTILS_TAG MINIFORGE_VER MINIFORGE_INSTALLATION_DIR
 do
     if [ -z ${var+x} ]
     then
@@ -38,27 +37,27 @@ if [[ $(uname) == "Darwin" ]]; then
     OS="MacOSX"
     
     # Remove existing installation on macOS runners
-    sudo rm -rf ${MAMBAFORGE_INSTALLATION_DIR}
-    sudo mkdir -p $(dirname $MAMBAFORGE_INSTALLATION_DIR)
-    sudo chown -R $USER $(dirname $MAMBAFORGE_INSTALLATION_DIR)
+    sudo rm -rf ${MINIFORGE_INSTALLATION_DIR}
+    sudo mkdir -p $(dirname $MINIFORGE_INSTALLATION_DIR)
+    sudo chown -R $USER $(dirname $MINIFORGE_INSTALLATION_DIR)
     
     # conda-forge-ci-setup does some additional setup for Mac.
     BIOCONDA_ADDITIONAL_INSTALL_PKGS="conda-forge-ci-setup"
 else
-    mkdir -p $(dirname $MAMBAFORGE_INSTALLATION_DIR)
+    mkdir -p $(dirname $MINIFORGE_INSTALLATION_DIR)
     OS="Linux"
     BIOCONDA_ADDITIONAL_INSTALL_PKGS=""
 fi
 
-MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/download/${MAMBAFORGE_VER}/Miniforge3-${MAMBAFORGE_VER}-${OS}-${ARCH}.sh"
+MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/download/${MINIFORGE_VER}/Miniforge3-${MINIFORGE_VER}-${OS}-${ARCH}.sh"
 
-# Install mambaforge
+# Install miniforge
 echo Download ${MINIFORGE_URL}
-curl -L ${MINIFORGE_URL} > mambaforge.sh
-head mambaforge.sh
-bash mambaforge.sh -b -p "${MAMBAFORGE_INSTALLATION_DIR}"
+curl -L ${MINIFORGE_URL} > miniforge.sh
+head miniforge.sh
+bash miniforge.sh -b -p "${MINIFORGE_INSTALLATION_DIR}"
 
-export PATH="${MAMBAFORGE_INSTALLATION_DIR}/bin:${PATH}"
+export PATH="${MINIFORGE_INSTALLATION_DIR}/bin:${PATH}"
 
 # Set up channels
 # disable build preparation here because we don't yet have the local channel from conda-build
@@ -69,8 +68,8 @@ BIOCONDA_DISABLE_BUILD_PREP=1 bash configure-conda.sh
 # which case set BIOCONDA_DISABLE_BUILD_PREP to a non-zero value.
 if [ ${BIOCONDA_DISABLE_BUILD_PREP:=0} == 0 ]; then
     
-    source ${MAMBAFORGE_INSTALLATION_DIR}/etc/profile.d/conda.sh
-    source ${MAMBAFORGE_INSTALLATION_DIR}/etc/profile.d/mamba.sh
+    source ${MINIFORGE_INSTALLATION_DIR}/etc/profile.d/conda.sh
+    source ${MINIFORGE_INSTALLATION_DIR}/etc/profile.d/mamba.sh
     
     # set up env with all dependencies
     conda create -n bioconda -y --file https://raw.githubusercontent.com/bioconda/bioconda-utils/$BIOCONDA_UTILS_TAG/bioconda_utils/bioconda_utils-requirements.txt $BIOCONDA_ADDITIONAL_INSTALL_PKGS
@@ -78,15 +77,15 @@ if [ ${BIOCONDA_DISABLE_BUILD_PREP:=0} == 0 ]; then
     conda activate bioconda
     
     # install bioconda-utils itself via pip (this way we don't always have to wait for the conda package to be built before being able to fix things here)
-    pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG
+    pip install git+https://github.com/bioconda/bioconda-utils.git@$BIOCONDA_UTILS_TAG --no-build-isolation --no-cache-dir --use-pep517
     
     # Set local channel as highest priority (requires conda-build, which is
     # installed as a dependency of bioconda-utils)
-    mkdir -p "${MAMBAFORGE_INSTALLATION_DIR}/conda-bld/{noarch,linux-64,osx-64,linux-aarch64,osx-arm64}"
+    mkdir -p "${MINIFORGE_INSTALLATION_DIR}/conda-bld/{noarch,linux-64,osx-64,linux-aarch64,osx-arm64}"
     # The base installation does not include conda-index, so use "command conda" to run bioconda
     # env's "conda index".
-    command conda index "${MAMBAFORGE_INSTALLATION_DIR}/conda-bld"
-    conda config --add channels "file://${MAMBAFORGE_INSTALLATION_DIR}/conda-bld"
+    command conda index "${MINIFORGE_INSTALLATION_DIR}/conda-bld"
+    conda config --add channels "file://${MINIFORGE_INSTALLATION_DIR}/conda-bld"
 fi
 
 echo "=========="
