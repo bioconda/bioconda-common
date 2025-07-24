2134bc05d51f44d1a7466d0f280e17736265848f
mencian <joshua.zhuang@yahoo.com>
2025-07-24 14:43:53 -0500

    Bump macOS arm64 SDK to 11.3

--- common.sh
+++ common.sh
@@ -6,8 +6,8 @@ platform=$(uname -s)
 arch=$(uname -m)
 if [[ "$platform" == "Darwin" ]]; then
     if [[ "$arch" == "arm64" ]]; then
-        export MACOSX_DEPLOYMENT_TARGET=11.0
-        export MACOSX_SDK_VERSION=11.0
+        export MACOSX_DEPLOYMENT_TARGET=11.3
+        export MACOSX_SDK_VERSION=11.3
     elif [[ "$arch" == "x86_64" ]]; then
         export MACOSX_DEPLOYMENT_TARGET=11.0
         export MACOSX_SDK_VERSION=11.0
