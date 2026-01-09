6d85649d2dd4935708768cce33d9812065fbecc4
Björn Grüning <bjoern@gruenings.eu>
2026-01-09 09:55:41 +0100

    update bioconda-utils

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 # Respect overrides from environment
-BIOCONDA_UTILS_TAG=${BIOCONDA_UTILS_TAG:="v3.9.1"}
-MINIFORGE_VER=${MINIFORGE_VER:="25.3.1-0"}
+BIOCONDA_UTILS_TAG=${BIOCONDA_UTILS_TAG:="v3.9.2"}
+MINIFORGE_VER=${MINIFORGE_VER:="25.11.0-1"}
 MINIFORGE_INSTALLATION_DIR=${MINIFORGE_INSTALLATION_DIR:="/opt/mambaforge"}
 platform=$(uname -s)
 arch=$(uname -m)
